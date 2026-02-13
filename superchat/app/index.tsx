9d8054c0101a29e7757bf62a1fdf001a53f515c5
heart47315-stack <heart47315@gmail.com>
2026-02-13 19:08:38 +0700

    พอเลิก

--- superchat/app/index.tsx
+++ superchat/app/index.tsx
@@ -1,10 +1,6 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'expo-router'
+import React from 'react'
+import { Redirect } from 'expo-router'
 
 export default function Index() {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/login')
-  }, [])
-  return null
+  return <Redirect href="/login" />
 }
